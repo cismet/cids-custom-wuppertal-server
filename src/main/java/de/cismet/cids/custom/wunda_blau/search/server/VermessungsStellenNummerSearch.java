b7fccf3e232f43105b585a073e503a3edfbda5c1
Daniel Meiers <daniel.meiers@cismet.de>
2013-11-14 11:43:04 +0100

    #32 jalopy

--- src/main/java/de/cismet/cids/custom/wunda_blau/search/server/VermessungsStellenNummerSearch.java
+++ src/main/java/de/cismet/cids/custom/wunda_blau/search/server/VermessungsStellenNummerSearch.java
@@ -1,12 +1,10 @@
-/**
- * *************************************************
- *
- * cismet GmbH, Saarbruecken, Germany
- * 
-* ... and it just works.
- * 
-***************************************************
- */
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -29,34 +27,38 @@ import de.cismet.cids.server.search.SearchException;
 /**
  * DOCUMENT ME!
  *
- * @author daniel
- * @version $Revision$, $Date$
+ * @author   daniel
+ * @version  $Revision$, $Date$
  */
 public class VermessungsStellenNummerSearch extends AbstractCidsServerSearch {
 
     //~ Static fields/initializers ---------------------------------------------
+
     private static final Logger LOG = Logger.getLogger(VermessungsStellenNummerSearch.class);
 
     //~ Instance fields --------------------------------------------------------
+
     private String userName;
-    private final String QUERY
-            = "select k.vermessungsstellennummer from \"public\".billing_kunden_logins kl join billing_kunde k on k.id=kl.kunde "
-            + "where vermessungsstellennummer is not null and kl.name like '%1$s';";
+    private final String QUERY =
+        "select k.vermessungsstellennummer from \"public\".billing_kunden_logins kl join billing_kunde k on k.id=kl.kunde "
+                + "where vermessungsstellennummer is not null and kl.name like '%1$s';";
 
     //~ Constructors -----------------------------------------------------------
+
     /**
      * Creates a new VermessungsStellenNummerSearch object.
      *
-     * @param userName DOCUMENT ME!
+     * @param  userName  DOCUMENT ME!
      */
     public VermessungsStellenNummerSearch(final String userName) {
         this.userName = userName;
     }
 
     //~ Methods ----------------------------------------------------------------
+
     @Override
     public Collection performServerSearch() throws SearchException {
-        final MetaService ms = (MetaService) getActiveLocalServers().get("WUNDA_BLAU");
+        final MetaService ms = (MetaService)getActiveLocalServers().get("WUNDA_BLAU");
 
         if (ms != null) {
             try {
@@ -67,7 +69,7 @@ public class VermessungsStellenNummerSearch extends AbstractCidsServerSearch {
                 final ArrayList<ArrayList> lists = ms.performCustomSearch(query);
                 final ArrayList<String> result = new ArrayList<String>();
                 for (final ArrayList l : lists) {
-                    final String vermessungsStellenNummer = (String) l.get(0);
+                    final String vermessungsStellenNummer = (String)l.get(0);
                     result.add(vermessungsStellenNummer);
                 }
                 return result;
