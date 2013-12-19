75c4aadb63392e0b907e1bda90f5a6cccad0be68
Daniel Meiers <daniel.meiers@cismet.de>
2013-12-19 16:58:29 +0100

    reduced log level if an error during parsing open oder log file happens. removed stack trace for those exception

--- src/main/java/de/cismet/cids/custom/utils/nas/NASProductGenerator.java
+++ src/main/java/de/cismet/cids/custom/utils/nas/NASProductGenerator.java
@@ -988,11 +988,11 @@ public class NASProductGenerator {
                 map.put(s, wrapperMap.get(s).getMap());
             }
         } catch (JsonParseException ex) {
-            log.error("Could not parse nas order log files", ex);
+            log.warn("Could not parse nas order log files");
         } catch (JsonMappingException ex) {
-            log.error("error while json mapping/unmarshalling of nas order log file", ex);
+            log.warn("error while json mapping/unmarshalling of nas order log file");
         } catch (IOException ex) {
-            log.error("error while loading nas order log file", ex);
+            log.warn("error while loading nas order log file");
         }
     }
 
