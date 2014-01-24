6e9e218ebf6410dd115fc741c7cccf83834ffb12
helllth <th@cismet.de>
2014-01-24 22:21:25 +0100

    fixes #42: seperation of startup hook functionality

---
+++ src/main/java/de/cismet/cids/custom/proxy/startuphooks/WuppProxyStartupHook.java
@@ -0,0 +1,43 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cismet.cids.custom.proxy.startuphooks;
+
+import Sirius.server.middleware.impls.proxy.ProxyStartupHook;
+
+import de.cismet.cids.custom.utils.alkis.AlkisConstants;
+import de.cismet.cids.custom.utils.alkis.AlkisProducts;
+import de.cismet.cids.custom.wunda_blau.search.actions.NasZaehlObjekteSearch;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   thorsten
+ * @version  $Revision$, $Date$
+ */
+@org.openide.util.lookup.ServiceProvider(service = ProxyStartupHook.class)
+public class WuppProxyStartupHook implements ProxyStartupHook {
+
+    //~ Methods ----------------------------------------------------------------
+
+    @Override
+    public void proxyStarted() {
+        final AlkisProducts pr = new AlkisProducts(
+                AlkisConstants.COMMONS.USER,
+                AlkisConstants.COMMONS.PASSWORD,
+                AlkisConstants.COMMONS.SERVICE);
+
+        final NasZaehlObjekteSearch search = new NasZaehlObjekteSearch(
+                null,
+                NasZaehlObjekteSearch.NasSearchType.ADRESSE);
+    }
+}
