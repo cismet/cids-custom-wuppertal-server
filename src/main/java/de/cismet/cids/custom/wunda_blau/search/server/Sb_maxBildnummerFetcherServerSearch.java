bff29846c5ab6d8e8e70a6edefc5e466f4f13c02
Gilles Baatz <gilles.baatz@cismet.de>
2014-04-01 11:53:20 +0200

    #49 added Sb_maxBildnummerFetcherServerSearch. A query fetching the highest image number with six digits.

---
+++ src/main/java/de/cismet/cids/custom/wunda_blau/search/server/Sb_maxBildnummerFetcherServerSearch.java
@@ -0,0 +1,47 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+package de.cismet.cids.custom.wunda_blau.search.server;
+
+import Sirius.server.middleware.interfaces.domainserver.MetaService;
+
+import java.rmi.RemoteException;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import de.cismet.cids.server.search.AbstractCidsServerSearch;
+import de.cismet.cids.server.search.SearchException;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   Gilles Baatz
+ * @version  $Revision$, $Date$
+ */
+public class Sb_maxBildnummerFetcherServerSearch extends AbstractCidsServerSearch {
+
+    //~ Static fields/initializers ---------------------------------------------
+
+    private static final String searchQuery =
+        "select max(bildnummer::int) from sb_stadtbild where bildnummer ~ '^\\\\d{6}$'";
+
+    //~ Methods ----------------------------------------------------------------
+
+    @Override
+    public Collection performServerSearch() throws SearchException {
+        final MetaService ms = (MetaService)getActiveLocalServers().get("WUNDA_BLAU");
+        if (ms != null) {
+            try {
+                final ArrayList<ArrayList> lists = ms.performCustomSearch(searchQuery);
+                return lists;
+            } catch (RemoteException ex) {
+            }
+        }
+        return null;
+    }
+}
