77d527b8957a8c3cddd71201038974be61b23c00
JeanAtCismet <jean.ruiz@cismet.de>
2017-01-09 19:27:08 +0100

    fixed #172

--- src/main/java/de/cismet/cids/custom/utils/vermessungsunterlagen/tasks/VermUntTaskPNR.java
+++ src/main/java/de/cismet/cids/custom/utils/vermessungsunterlagen/tasks/VermUntTaskPNR.java
@@ -16,6 +16,8 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 
+import java.nio.charset.Charset;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -90,7 +92,7 @@ public class VermUntTaskPNR extends VermessungsunterlagenTask {
 
                             final String filename = getPath() + "/" + auftragsnummer + "_"
                                         + bean.getUtmKilometerQuadrat() + ".txt";
-                            FileUtils.writeStringToFile(new File(filename), protokoll);
+                            FileUtils.writeStringToFile(new File(filename), protokoll, Charset.forName("UTF-8"));
                         }
                         first = false;
                     } catch (final Exception exception) {
