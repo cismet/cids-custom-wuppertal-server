a95d9a86ca8056491944c3144f0e90fdef60de61
Jean-Michel Ruiz <jean.ruiz@cismet.de>
2018-12-17 19:04:46 +0100

    fixed #233

--- src/main/java/de/cismet/cids/custom/wunda_blau/startuphooks/MotdWundaStartupHook.java
+++ src/main/java/de/cismet/cids/custom/wunda_blau/startuphooks/MotdWundaStartupHook.java
@@ -12,17 +12,13 @@
  */
 package de.cismet.cids.custom.wunda_blau.startuphooks;
 
-import Sirius.server.middleware.impls.domainserver.DomainServerImpl;
 import Sirius.server.middleware.interfaces.domainserver.DomainServerStartupHook;
 
-import de.cismet.cids.custom.utils.motd.MotdRetriever;
-import de.cismet.cids.custom.utils.motd.MotdRetrieverListener;
-import de.cismet.cids.custom.utils.motd.MotdRetrieverListenerEvent;
+import java.util.Properties;
 
-import de.cismet.cids.server.messages.CidsServerMessageManagerImpl;
+import de.cismet.cids.custom.utils.WundaBlauServerResources;
 
-import de.cismet.connectioncontext.ConnectionContext;
-import de.cismet.connectioncontext.ConnectionContextStore;
+import de.cismet.cids.utils.serverresources.ServerResourcesLoader;
 
 /**
  * DOCUMENT ME!
@@ -31,95 +27,9 @@ import de.cismet.connectioncontext.ConnectionContextStore;
  * @version  $Revision$, $Date$
  */
 @org.openide.util.lookup.ServiceProvider(service = DomainServerStartupHook.class)
-public class MotdWundaStartupHook implements DomainServerStartupHook, ConnectionContextStore {
-
-    //~ Static fields/initializers ---------------------------------------------
-
-    private static final transient org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(
-            MotdWundaStartupHook.class);
-
-    // Title of the day
-    public static final String MOTD_MESSAGE_TOTD = "totd";
-    public static final String MOTD_MESSAGE_TOTD_EXTERN = "totd_extern";
-
-    // Message of the day
-    public static final String MOTD_MESSAGE_MOTD = "motd";
-    public static final String MOTD_MESSAGE_MOTD_EXTERN = "motd_extern";
-
-    //~ Instance fields --------------------------------------------------------
-
-    private ConnectionContext connectionContext = ConnectionContext.createDummy();
+public class MotdWundaStartupHook extends MotdStartupHook {
 
     //~ Methods ----------------------------------------------------------------
-
-    @Override
-    public void initWithConnectionContext(final ConnectionContext connectionContext) {
-        this.connectionContext = connectionContext;
-    }
-
-    @Override
-    public void domainServerStarted() {
-        new Thread(new Runnable() {
-
-                @Override
-                public void run() {
-                    while (DomainServerImpl.getServerInstance() == null) {
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException ex) {
-                        }
-                    }
-
-                    try {
-                        if (MotdRetriever.getInstance().init(getDomain())) {
-                            MotdRetriever.getInstance().addMotdRetrieverListener(new MotdRetrieverListener() {
-
-                                    @Override
-                                    public void totdChanged(final MotdRetrieverListenerEvent event) {
-                                        if (event.isExtern()) {
-                                            CidsServerMessageManagerImpl.getInstance()
-                                                    .publishMessage(
-                                                        MOTD_MESSAGE_TOTD_EXTERN,
-                                                        event.getContent(),
-                                                        true,
-                                                        getConnectionContext());
-                                        } else {
-                                            CidsServerMessageManagerImpl.getInstance()
-                                                    .publishMessage(
-                                                        MOTD_MESSAGE_TOTD,
-                                                        event.getContent(),
-                                                        true,
-                                                        getConnectionContext());
-                                        }
-                                    }
-
-                                    @Override
-                                    public void motdChanged(final MotdRetrieverListenerEvent event) {
-                                        if (event.isExtern()) {
-                                            CidsServerMessageManagerImpl.getInstance()
-                                                    .publishMessage(
-                                                        MOTD_MESSAGE_MOTD_EXTERN,
-                                                        event.getContent(),
-                                                        false,
-                                                        getConnectionContext());
-                                        } else {
-                                            CidsServerMessageManagerImpl.getInstance()
-                                                    .publishMessage(
-                                                        MOTD_MESSAGE_MOTD,
-                                                        event.getContent(),
-                                                        false,
-                                                        getConnectionContext());
-                                        }
-                                    }
-                                });
-                            MotdRetriever.getInstance().start();
-                        }
-                    } catch (final Exception ex) {
-                        LOG.warn("Error while initializing the MotdRetriever !", ex);
-                    }
-                }
-            }).start();
-    }
 
     @Override
     public String getDomain() {
@@ -127,7 +37,7 @@ public class MotdWundaStartupHook implements DomainServerStartupHook, Connection
     }
 
     @Override
-    public ConnectionContext getConnectionContext() {
-        return connectionContext;
+    public Properties getProperties() throws Exception {
+        return ServerResourcesLoader.getInstance().loadProperties(WundaBlauServerResources.MOTD_PROPERTIES.getValue());
     }
 }
