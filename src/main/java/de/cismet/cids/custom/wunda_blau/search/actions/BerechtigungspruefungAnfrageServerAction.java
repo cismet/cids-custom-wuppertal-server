2635b9c7ff445146867c67d46d4ed4897c35fa89
Jean-Michel Ruiz <jean.ruiz@cismet.de>
2020-01-29 13:44:24 +0100

    issue #255 creating billing entries in serverAction

--- src/main/java/de/cismet/cids/custom/wunda_blau/search/actions/BerechtigungspruefungAnfrageServerAction.java
+++ src/main/java/de/cismet/cids/custom/wunda_blau/search/actions/BerechtigungspruefungAnfrageServerAction.java
@@ -12,13 +12,21 @@
  */
 package de.cismet.cids.custom.wunda_blau.search.actions;
 
+import Sirius.server.middleware.impls.domainserver.DomainServerImpl;
 import Sirius.server.middleware.interfaces.domainserver.MetaService;
 import Sirius.server.middleware.interfaces.domainserver.MetaServiceStore;
 import Sirius.server.newuser.User;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import de.cismet.cids.custom.utils.berechtigungspruefung.BerechtigungspruefungBillingDownloadInfo;
 import de.cismet.cids.custom.utils.berechtigungspruefung.BerechtigungspruefungDownloadInfo;
 import de.cismet.cids.custom.utils.berechtigungspruefung.BerechtigungspruefungHandler;
+import de.cismet.cids.custom.utils.billing.BillingModus;
+import de.cismet.cids.custom.utils.billing.BillingPrice;
+import de.cismet.cids.custom.utils.billing.BillingProduct;
+import de.cismet.cids.custom.utils.billing.BillingUsage;
+import de.cismet.cids.custom.utils.billing.BillingUtils;
 
 import de.cismet.cids.dynamics.CidsBean;
 
@@ -58,7 +66,8 @@ public class BerechtigungspruefungAnfrageServerAction implements UserAwareServer
 
         //~ Enum constants -----------------------------------------------------
 
-        ABGEHOLT, DATEINAME, BEGRUENDUNG, BERECHTIGUNGSGRUND, DOWNLOADINFO_JSON
+        ABGEHOLT, DATEINAME, BEGRUENDUNG, BERECHTIGUNGSGRUND, DOWNLOADINFO_JSON, BILLING_BERECHNUNG, BILLING_USAGE,
+        BILLING_PRODUCT, BILLING_MODUS, BILLING_PRICE
     }
 
     //~ Instance fields --------------------------------------------------------
@@ -83,6 +92,11 @@ public class BerechtigungspruefungAnfrageServerAction implements UserAwareServer
             String downloadinfoJson = null;
             String berechtigungsgrund = null;
             String abgeholt = null;
+            String billingBerechnung = null;
+            BillingPrice billingPrice = null;
+            BillingModus billingModus = null;
+            BillingProduct billingProduct = null;
+            BillingUsage billingUsage = null;
             if (params != null) {
                 for (final ServerActionParameter sap : params) {
                     if (sap.getKey().equals(ParameterType.DATEINAME.toString())) {
@@ -95,6 +109,16 @@ public class BerechtigungspruefungAnfrageServerAction implements UserAwareServer
                         downloadinfoJson = (String)sap.getValue();
                     } else if (sap.getKey().equals(ParameterType.ABGEHOLT.toString())) {
                         abgeholt = (String)sap.getValue();
+                    } else if (sap.getKey().equals(ParameterType.BILLING_BERECHNUNG.toString())) {
+                        billingBerechnung = (String)sap.getValue();
+                    } else if (sap.getKey().equals(ParameterType.BILLING_PRICE.toString())) {
+                        billingPrice = (BillingPrice)sap.getValue();
+                    } else if (sap.getKey().equals(ParameterType.BILLING_USAGE.toString())) {
+                        billingUsage = (BillingUsage)sap.getValue();
+                    } else if (sap.getKey().equals(ParameterType.BILLING_PRODUCT.toString())) {
+                        billingProduct = (BillingProduct)sap.getValue();
+                    } else if (sap.getKey().equals(ParameterType.BILLING_MODUS.toString())) {
+                        billingModus = (BillingModus)sap.getValue();
                     }
                 }
             }
@@ -102,6 +126,7 @@ public class BerechtigungspruefungAnfrageServerAction implements UserAwareServer
             if (abgeholt != null) {
                 BerechtigungspruefungHandler.getInstance().setMetaService(getMetaService());
                 BerechtigungspruefungHandler.getInstance().closeAnfrage(user, abgeholt);
+                return null;
             } else {
                 final byte[] data = (byte[])body;
                 if ((body != null) && !(body instanceof byte[])) {
@@ -120,42 +145,54 @@ public class BerechtigungspruefungAnfrageServerAction implements UserAwareServer
                 final String schluessel = BerechtigungspruefungHandler.getInstance()
                             .createNewSchluessel(getUser(), downloadInfo);
 
+                final CidsBean anfrageBean = BerechtigungspruefungHandler.getInstance()
+                            .addNewAnfrage(
+                                getUser(),
+                                schluessel,
+                                downloadInfo,
+                                berechtigungsgrund,
+                                begruendung,
+                                dateiname,
+                                data);
+
                 if (downloadInfo instanceof BerechtigungspruefungBillingDownloadInfo) {
                     final BerechtigungspruefungBillingDownloadInfo billingDownloadInfo =
                         (BerechtigungspruefungBillingDownloadInfo)downloadInfo;
+
                     billingDownloadInfo.setAuftragsnummer(schluessel);
-                    final Integer billingId = billingDownloadInfo.getBillingId();
 
-                    final CidsBean billingBean = BerechtigungspruefungHandler.getInstance()
-                                .loadBillingBean(user, billingId);
+                    final CidsBean cb = BillingUtils.getInstance()
+                                .createBilling(
+                                    billingDownloadInfo.getProduktbezeichnung(),
+                                    schluessel,
+                                    "no.yet",
+                                    billingBerechnung,
+                                    billingModus,
+                                    billingUsage,
+                                    billingProduct,
+                                    billingPrice,
+                                    getUser(),
+                                    getMetaService(),
+                                    getConnectionContext());
 
-                    try {
-                        billingBean.setProperty("geschaeftsbuchnummer", schluessel);
-                        getMetaService().updateMetaObject(
-                            getUser(),
-                            billingBean.getMetaObject(),
-                            getConnectionContext());
-                    } catch (Exception ex) {
-                        LOG.error("Error while setting 'storniert' of billing", ex);
-                    }
+                    final CidsBean persistedBillingBean = DomainServerImpl.getServerInstance()
+                                .insertMetaObject(getUser(), cb.getMetaObject(), getConnectionContext())
+                                .getBean();
+
+                    billingDownloadInfo.setBillingId(persistedBillingBean.getPrimaryKeyValue());
+                    anfrageBean.setProperty(
+                        "downloadinfo_json",
+                        new ObjectMapper().writeValueAsString(billingDownloadInfo));
+                    DomainServerImpl.getServerInstance()
+                            .updateMetaObject(getUser(), anfrageBean.getMetaObject(), getConnectionContext());
                 }
-
-                BerechtigungspruefungHandler.getInstance()
-                        .addNewAnfrage(
-                            getUser(),
-                            schluessel,
-                            downloadInfo,
-                            berechtigungsgrund,
-                            begruendung,
-                            dateiname,
-                            data);
 
                 return schluessel;
             }
         } catch (final Exception ex) {
             LOG.error("error while executing anfrage task", ex);
+            return ex;
         }
-        return null;
     }
 
     @Override
