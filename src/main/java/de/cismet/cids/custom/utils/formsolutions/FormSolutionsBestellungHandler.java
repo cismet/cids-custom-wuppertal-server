7e8ec283566f069dd8db34e0a1f470bdfda53dca
Jean-Michel Ruiz <jean.ruiz@cisnet.de>
2020-06-08 15:56:53 +0200

    fixed #255 skipping billing for BAB_WEITERLEITUNG

--- src/main/java/de/cismet/cids/custom/utils/formsolutions/FormSolutionsBestellungHandler.java
+++ src/main/java/de/cismet/cids/custom/utils/formsolutions/FormSolutionsBestellungHandler.java
@@ -691,35 +691,43 @@ public class FormSolutionsBestellungHandler implements ConnectionContextProvider
             for (final String transid : new ArrayList<>(fsBeanMap.keySet())) {
                 final CidsBean bestellungBean = fsBeanMap.get(transid);
                 if (bestellungBean != null) {
-                    try {
-                        if (!Boolean.TRUE.equals(bestellungBean.getProperty("duplicate"))
-                                    && (bestellungBean.getProperty("gutschein_code") == null)) {
-                            if (bestellungBean.getProperty("fk_billing") == null) {
-                                final CidsBean billingBean = doBilling(
-                                        bestellungBean,
-                                        (downloadInfoMap != null) ? downloadInfoMap.get(transid) : null,
-                                        transid);
-                                if (billingBean != null) {
-                                    bestellungBean.setProperty("fk_billing", billingBean);
-                                    getMetaService().updateMetaObject(
-                                        getUser(),
-                                        bestellungBean.getMetaObject(),
-                                        getConnectionContext());
+                    final ProductType productType = determineProductType(bestellungBean);
+                    switch (productType) {
+                        case SGK:
+                        case ABK:
+                        case BAB_ABSCHLUSS: {
+                            try {
+                                if (!Boolean.TRUE.equals(bestellungBean.getProperty("duplicate"))
+                                            && (bestellungBean.getProperty("gutschein_code") == null)) {
+                                    if (bestellungBean.getProperty("fk_billing") == null) {
+                                        final CidsBean billingBean = doBilling(
+                                                bestellungBean,
+                                                (downloadInfoMap != null) ? downloadInfoMap.get(transid) : null,
+                                                transid);
+                                        if (billingBean != null) {
+                                            bestellungBean.setProperty("fk_billing", billingBean);
+                                            getMetaService().updateMetaObject(
+                                                getUser(),
+                                                bestellungBean.getMetaObject(),
+                                                getConnectionContext());
+                                        }
+                                    }
                                 }
+                            } catch (final Exception ex) {
+                                setErrorStatus(
+                                    transid,
+                                    STATUS_BILLING,
+                                    bestellungBean,
+                                    "Fehler beim Erzeugen des Billings",
+                                    ex);
                             }
                         }
-                    } catch (final Exception ex) {
-                        setErrorStatus(
-                            transid,
-                            STATUS_BILLING,
-                            bestellungBean,
-                            "Fehler beim Erzeugen des Billings",
-                            ex);
                     }
                 }
             }
         }
     }
+    
     /**
      * DOCUMENT ME!
      *
