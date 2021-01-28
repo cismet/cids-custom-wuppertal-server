008bef20106eb170eeecbb61041fe0a13557a9f9
sandra-simmert <sandra.simmert@stadt.wuppertal.de>
2021-01-28 05:45:53 +0100

    Kategorien fuer Poi

---
+++ src/main/java/de/cismet/cids/custom/tostringconverter/wunda_blau/PoiKategorienToStringConverter.java
@@ -0,0 +1,42 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cismet.cids.custom.tostringconverter.wunda_blau;
+
+import de.cismet.cids.tools.CustomToStringConverter;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   sandra
+ * @version  $Revision$, $Date$
+ */
+public class PoiKategorienToStringConverter extends CustomToStringConverter {
+
+    //~ Static fields/initializers ---------------------------------------------
+
+    public static final String FIELD__NAME = "name"; 
+    public static final String FIELD__NUMMER = "nummer"; 
+    public static final String FIELD__ID = "id";                 
+
+    //~ Methods ----------------------------------------------------------------
+
+    @Override
+    public String createString() {
+        final String myid = String.valueOf(cidsBean.getProperty(FIELD__ID));
+        if ("-1".equals(myid)) {
+            return String.valueOf(cidsBean.getProperty(FIELD__NAME));
+        } else {
+            return String.valueOf(cidsBean.getProperty(FIELD__NUMMER)) + "  "  + String.valueOf(cidsBean.getProperty(FIELD__NAME));
+        }
+    }
+}
