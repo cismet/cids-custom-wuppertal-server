cd34b90697f8899f26ee38db0b5f30ec36c3bea7
sandra-simmert <sandra.simmert@stadt.wuppertal.de>
2021-06-23 09:13:23 +0200

    DeletionProvider Krone, Stamm, Wurzel & Massnahme

---
+++ src/main/java/de/cismet/cids/custom/deletionprovider/BaumMassnahmeDeletionProvider.java
@@ -0,0 +1,94 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+package de.cismet.cids.custom.deletionprovider;
+
+import Sirius.server.localserver.object.AbstractCustomDeletionProvider;
+import Sirius.server.localserver.object.CustomDeletionProvider;
+import Sirius.server.localserver.object.DeletionProviderClientException;
+import Sirius.server.middleware.types.MetaObject;
+import Sirius.server.newuser.User;
+
+import org.apache.log4j.Logger;
+
+import org.openide.util.lookup.ServiceProvider;
+
+import de.cismet.cids.dynamics.CidsBean;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import org.openide.util.Exceptions;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   sandra
+ * @version  $Revision$, $Date$
+ */
+@ServiceProvider(service = CustomDeletionProvider.class)
+public class BaumMassnahmeDeletionProvider extends AbstractCustomDeletionProvider {
+
+    //~ Static fields/initializers ---------------------------------------------
+
+    private static final Logger LOG = Logger.getLogger(BaumMassnahmeDeletionProvider.class);
+    public static final String TABLE_NAME = "baum_massnahme";
+    public static final String FIELD__ID = "id";
+    public static final String FIELD__FK = "fk_massnahme";
+    public static final String TABLE_NAME_SEARCH_S = "baum_schaden_massnahme";
+    public String DELETE_TEXT = "Diese Maßnahme kann nicht gelöscht werden, da diese verwendet wird.";
+    public boolean notToDelete = false;
+
+    //~ Methods ----------------------------------------------------------------
+
+    @Override
+    public String getTableName() {
+        return TABLE_NAME;
+    }
+
+    @Override
+    public boolean isMatching(final User user, final MetaObject metaObject) {
+        if (metaObject != null) {
+            final CidsBean schadenBean = metaObject.getBean();
+            final Integer schaden_id = (Integer) schadenBean.getProperty(FIELD__ID);
+            
+            final String queryMassnahmeInSchaden = ""
+                        + "SELECT * "
+                        + "FROM "
+                        + TABLE_NAME_SEARCH_S
+                        + " WHERE "
+                        + FIELD__FK
+                        + " = "
+                        + schaden_id
+                        + ";"; 
+            
+            try {
+                ArrayList<ArrayList>artArrayS = getMetaService().performCustomSearch(queryMassnahmeInSchaden, getConnectionContext());
+                if (artArrayS.size() < 1) {
+                    return false; // kein true sonst läuft jede Klasse durch
+                }else {
+                    notToDelete = true;
+                }
+            } catch (RemoteException ex) {
+                Exceptions.printStackTrace(ex);
+            }
+        }
+        return super.isMatching(user, metaObject);
+    }
+
+    @Override
+    public boolean customDeleteMetaObject(final User user, final MetaObject metaObject) throws Exception {
+        if (metaObject != null) {
+            // darf nicht geloescht werden
+            
+            if (notToDelete) {
+                throw new DeletionProviderClientException(
+                        DELETE_TEXT);
+             
+            }
+        }
+        return false;
+    }
+}
