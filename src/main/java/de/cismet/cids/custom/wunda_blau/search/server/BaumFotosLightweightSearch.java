e22b49e82dfcd1dc1b50c4b34c7b77146858c93f
sandra-simmert <sandra.simmert@stadt.wuppertal.de>
2022-08-11 09:17:31 +0200

    FotoSearch

---
+++ src/main/java/de/cismet/cids/custom/wunda_blau/search/server/BaumFotosLightweightSearch.java
@@ -0,0 +1,195 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+package de.cismet.cids.custom.wunda_blau.search.server;
+
+import Sirius.server.middleware.interfaces.domainserver.MetaService;
+import Sirius.server.middleware.types.MetaClass;
+import Sirius.server.middleware.types.MetaObjectNode;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import org.apache.log4j.Logger;
+
+import org.openide.util.lookup.ServiceProvider;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import de.cismet.cids.dynamics.CidsBean;
+
+import de.cismet.cids.server.search.AbstractCidsServerSearch;
+import de.cismet.cids.server.search.SearchException;
+
+import de.cismet.cidsx.base.types.Type;
+
+import de.cismet.cidsx.server.api.types.SearchInfo;
+import de.cismet.cidsx.server.api.types.SearchParameterInfo;
+import de.cismet.cidsx.server.search.RestApiCidsServerSearch;
+import de.cismet.cidsx.server.search.builtin.legacy.LightweightMetaObjectsSearch;
+
+import de.cismet.connectioncontext.ConnectionContext;
+import de.cismet.connectioncontext.ConnectionContextStore;
+
+/**
+ * Builtin Legacy Search to delegate the operation getLightweightMetaObjectsByQuery to the cids Pure REST Search API.
+ *
+ * @author   Sandra Simmert
+ * @version  $Revision$, $Date$
+ */
+@ServiceProvider(service = RestApiCidsServerSearch.class)
+public class BaumFotosLightweightSearch extends AbstractCidsServerSearch implements RestApiCidsServerSearch,
+    LightweightMetaObjectsSearch,
+    ConnectionContextStore {
+
+    //~ Static fields/initializers ---------------------------------------------
+
+    private static final Logger LOG = Logger.getLogger(BaumFotosLightweightSearch.class);
+    private static final String TABLE_FOTOS = "baum_fotos";
+    private static final String FIELD__GEBIET = "baum_fotos";
+
+    //~ Instance fields --------------------------------------------------------
+
+    private ConnectionContext connectionContext = ConnectionContext.createDummy();
+
+    @Getter private final SearchInfo searchInfo;
+    @Getter @Setter private Integer gebietId;
+    @Getter @Setter private String representationPattern;
+    @Getter @Setter private String[] representationFields;
+
+    //~ Constructors -----------------------------------------------------------
+
+    /**
+     * Creates a new LightweightMetaObjectsByQuerySearch object.
+     */
+    public BaumFotosLightweightSearch() {
+        this.searchInfo = new SearchInfo(
+                this.getClass().getName(),
+                this.getClass().getSimpleName(),
+                "Builtin Legacy Search to delegate the operation getLightweightMetaObjectsByQuery to the cids Pure REST Search API.",
+                Arrays.asList(
+                    new SearchParameterInfo[] {
+                        new MySearchParameterInfo(FIELD__GEBIET, Type.INTEGER),
+                        new MySearchParameterInfo("representationPattern", Type.STRING, true),
+                        new MySearchParameterInfo("representationFields", Type.STRING, true)
+                    }),
+                new MySearchParameterInfo("return", Type.ENTITY_REFERENCE, true));
+    }
+
+    /**
+     * Creates a new BaumFotosLightweightSearch object.
+     *
+     * @param  representationPattern  DOCUMENT ME!
+     * @param  representationFields   DOCUMENT ME!
+     */
+    public BaumFotosLightweightSearch(
+            final String representationPattern,
+            final String[] representationFields) {
+        this();
+        setRepresentationPattern(representationPattern);
+        setRepresentationFields(representationFields);
+    }
+
+    //~ Methods ----------------------------------------------------------------
+
+    @Override
+    public void initWithConnectionContext(final ConnectionContext connectionContext) {
+        this.connectionContext = connectionContext;
+    }
+
+    @Override
+    public ConnectionContext getConnectionContext() {
+        return connectionContext;
+    }
+
+    @Override
+    public Collection performServerSearch() throws SearchException {
+        final Integer id = getGebietId();
+
+        final MetaService metaService = (MetaService)this.getActiveLocalServers().get("WUNDA_BLAU");
+        if (metaService == null) {
+            final String message = "Lightweight Meta Objects By Query Search "
+                        + "could not connect ot MetaService @domain 'WUNDA_BLAU'";
+            LOG.error(message);
+            throw new SearchException(message);
+        }
+
+        final Collection<String> conditions = new ArrayList<>();
+        if (id != null) {
+            conditions.add(String.format("%s = %d", FIELD__GEBIET, id));
+        }
+
+        final String query = String.format("SELECT ("
+                        + "SELECT c.id FROM cs_class c WHERE table_name ILIKE '%1$s') AS class_id, "
+                        + "id, %2$s FROM %1$s %3$s"
+                        + " ORDER BY %2$s",
+                TABLE_FOTOS,
+                representationFields[0],
+                (conditions.isEmpty() ? "" : (" WHERE " + String.join(" AND ", conditions))));
+        LOG.info(query);
+        try {
+            final MetaClass mc = CidsBean.getMetaClassFromTableName(
+                    "WUNDA_BLAU",
+                    TABLE_FOTOS,
+                    getConnectionContext());
+
+            // final MetaService ms = (MetaService)getActiveLocalServers().get("WUNDA_BLAU");
+            final List<MetaObjectNode> mons = new ArrayList<>();
+            final List<ArrayList> resultList = metaService.performCustomSearch(query, getConnectionContext());
+            for (final ArrayList al : resultList) {
+                final int cid = (Integer)al.get(0);
+                final int oid = (Integer)al.get(1);
+                final String name = String.valueOf(al.get(2));
+                final MetaObjectNode mon = new MetaObjectNode("WUNDA_BLAU", oid, cid, name, null, null);
+
+                mons.add(mon);
+            }
+            return mons;
+        } catch (final Exception ex) {
+            throw new SearchException("error while loading lwmos", ex);
+        }
+    }
+
+    //~ Inner Classes ----------------------------------------------------------
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @version  $Revision$, $Date$
+     */
+    private class MySearchParameterInfo extends SearchParameterInfo {
+
+        //~ Constructors -------------------------------------------------------
+
+        /**
+         * Creates a new MySearchParameterInfo object.
+         *
+         * @param  key   DOCUMENT ME!
+         * @param  type  DOCUMENT ME!
+         */
+        private MySearchParameterInfo(final String key, final Type type) {
+            this(key, type, null);
+        }
+        /**
+         * Creates a new MySearchParameterInfo object.
+         *
+         * @param  key    DOCUMENT ME!
+         * @param  type   DOCUMENT ME!
+         * @param  array  DOCUMENT ME!
+         */
+        private MySearchParameterInfo(final String key, final Type type, final Boolean array) {
+            super.setKey(key);
+            super.setType(type);
+            if (array != null) {
+                super.setArray(array);
+            }
+        }
+    }
+}
