b03d62960a8f264c9527d161a5bbea17678578a5
Jean-Michel Ruiz <jean.ruiz@cismet.de>
2022-11-28 16:17:22 +0100

    issue #336 backend changes for anonymous orders

--- src/main/java/de/cismet/cids/custom/wunda_blau/search/server/BufferingGeosearch.java
+++ src/main/java/de/cismet/cids/custom/wunda_blau/search/server/BufferingGeosearch.java
@@ -17,6 +17,8 @@ import org.apache.log4j.Logger;
 
 import org.openide.util.lookup.ServiceProvider;
 
+import java.util.Locale;
+
 import de.cismet.cids.server.search.builtin.DefaultGeoSearch;
 import de.cismet.cids.server.search.builtin.GeoSearch;
 
@@ -36,9 +38,60 @@ public final class BufferingGeosearch extends DefaultGeoSearch {
 
     /** LOGGER. */
     private static final transient Logger LOG = Logger.getLogger(BufferingGeosearch.class);
-    private static final String INTERSECTS_BUFFER = SearchProperties.getInstance().getIntersectsBuffer();
+
+    //~ Enums ------------------------------------------------------------------
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @version  $Revision$, $Date$
+     */
+    public static enum GeomMode {
+
+        //~ Enum constants -----------------------------------------------------
+
+        INTERSECTS, WITHIN, CONTAINS
+    }
+
+    //~ Instance fields --------------------------------------------------------
+
+    private GeomMode geomMode = GeomMode.INTERSECTS;
+    private Double buffer;
+
+    //~ Constructors -----------------------------------------------------------
+
+    /**
+     * Creates a new BufferingGeosearch object.
+     */
+    public BufferingGeosearch() {
+        if (SearchProperties.getInstance().getIntersectsBuffer() != null) {
+            try {
+                buffer = Double.parseDouble(SearchProperties.getInstance().getIntersectsBuffer());
+            } catch (final Exception ex) {
+                LOG.error("error while parsing IntersectsBuffer from SearchProperties to double", ex);
+            }
+        }
+    }
 
     //~ Methods ----------------------------------------------------------------
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param  buffer  DOCUMENT ME!
+     */
+    public void setBuffer(final Double buffer) {
+        this.buffer = buffer;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param  geomMode  DOCUMENT ME!
+     */
+    public void setGeomMode(final GeomMode geomMode) {
+        this.geomMode = geomMode;
+    }
 
     @Override
     public PreparableStatement getSearchSql(final String domainKey) {
@@ -61,24 +114,52 @@ public final class BufferingGeosearch extends DefaultGeoSearch {
                     + "AND             i.attr_object_id = g.id "                                                   // NOI18N
                     + "AND i.class_id IN <cidsClassesInStatement> "                                                // NOI18N
                     + "AND geo_field && st_geomfromtext('SRID=<cidsSearchGeometrySRID>;<cidsSearchGeometryWKT>') " // NOI18N
-                    + "AND <intersectsStatement> "                                                                 // NOI18N
-                    + "ORDER BY        1,2,3";                                                                     // NOI18N
+                    + "AND <geomStatement> "                                                                       // NOI18N
+                    + "ORDER BY 1,2,3";                                                                            // NOI18N
 
         final Geometry searchGeometry = getGeometry();
-        final String intersectsStatement;
-        if (searchGeometry.getSRID() == 4326) {
-            intersectsStatement =
-                "st_intersects(geo_field,st_geomfromtext('SRID=<cidsSearchGeometrySRID>;<cidsSearchGeometryWKT>'))"; // NOI18N
-        } else {
-            if ((searchGeometry instanceof Polygon) || (searchGeometry instanceof MultiPolygon)) {                   // with buffer for searchGeometry
-                intersectsStatement = "st_intersects(st_buffer(geo_field, " + INTERSECTS_BUFFER
-                            + "),st_buffer(st_geomfromtext('SRID=<cidsSearchGeometrySRID>;<cidsSearchGeometryWKT>'), "
-                            + INTERSECTS_BUFFER + "))";                                                              // NOI18N
-            } else {                                                                                                 // without buffer for searchGeometry
-                intersectsStatement = "st_intersects(st_buffer(geo_field, " + INTERSECTS_BUFFER
-                            + "),st_geomfromtext('SRID=<cidsSearchGeometrySRID>;<cidsSearchGeometryWKT>'))";         // NOI18N
+        final String geomFunction;
+        switch (geomMode) {
+            case CONTAINS: {
+                geomFunction = "st_contains";
+            }
+            break;
+            case WITHIN: {
+                geomFunction = "st_within";
+            }
+            break;
+            case INTERSECTS:
+            default: {
+                geomFunction = "st_intersects";
             }
         }
+        final String searchGeomFromText = "st_geomfromtext('SRID=<cidsSearchGeometrySRID>;<cidsSearchGeometryWKT>')";
+        final String bufferedSearchGeomFromText = (buffer != null)
+            ? String.format(Locale.US, "st_buffer(%s, %f)", searchGeomFromText, buffer) : searchGeomFromText;
+        final String bufferedTargetGeomText = (buffer != null)
+            ? String.format(Locale.US, "st_buffer(geo_field, %f)", buffer) : "geo_field";
+
+        final String geomFunctionString = "%s(%s, %s)";
+        final String geomFunctionParamA;
+        final String geomFunctionParamB;
+        if (searchGeometry.getSRID() == 4326) {
+            geomFunctionParamA = "geo_field";
+            geomFunctionParamB = searchGeomFromText;
+        } else {
+            if ((searchGeometry instanceof Polygon) || (searchGeometry instanceof MultiPolygon)) {    // with buffer for searchGeometry
+                geomFunctionParamA = bufferedTargetGeomText;
+                geomFunctionParamB = bufferedSearchGeomFromText;
+            } else {                                                                                  // without buffer for searchGeometry
+                geomFunctionParamA = bufferedTargetGeomText;
+                geomFunctionParamB = searchGeomFromText;
+            }
+        }
+        final String geomStatement = String.format(
+                geomFunctionString,
+                geomFunction,
+                geomFunctionParamA,
+                geomFunctionParamB);
+
         final String cidsSearchGeometryWKT = searchGeometry.toText();
         final String sridString = Integer.toString(searchGeometry.getSRID());
         final String classesInStatement = getClassesInSnippetsPerDomain().get(domainKey);
@@ -91,18 +172,6 @@ public final class BufferingGeosearch extends DefaultGeoSearch {
 
             return null;
         }
-        if (LOG.isDebugEnabled()) {
-            LOG.debug("cidsClassesInStatement=" + classesInStatement);   // NOI18N
-        }
-        if (LOG.isDebugEnabled()) {
-            LOG.debug("cidsSearchGeometryWKT=" + cidsSearchGeometryWKT); // NOI18N
-        }
-        if (LOG.isDebugEnabled()) {
-            LOG.debug("cidsSearchGeometrySRID=" + sridString);           // NOI18N
-        }
-        if (LOG.isDebugEnabled()) {
-            LOG.debug("intersectsStatement=" + intersectsStatement);     // NOI18N
-        }
 
         if ((classesInStatement == null) || (classesInStatement.trim().length() == 0)) {
             LOG.warn("There are no search classes defined for domain '" + domainKey // NOI18N
@@ -112,7 +181,7 @@ public final class BufferingGeosearch extends DefaultGeoSearch {
         }
 
         final PreparableStatement ps = new PreparableStatement(
-                sql.replaceAll("<intersectsStatement>", intersectsStatement)  // NOI18N
+                sql.replaceAll("<geomStatement>", geomStatement)              // NOI18N
                 .replaceAll("<cidsClassesInStatement>", classesInStatement)   // NOI18N
                 .replaceAll("<cidsSearchGeometryWKT>", cidsSearchGeometryWKT) // NOI18N
                 .replaceAll("<cidsSearchGeometrySRID>", sridString),
