69289e556c13290d665f03b61f61b51c003424ff
sandra-simmert <sandra.simmert@stadt.wuppertal.de>
2023-01-05 06:57:42 +0100

    Klimaroute conf

--- src/main/java/de/cismet/cids/custom/utils/WundaBlauServerResources.java
+++ src/main/java/de/cismet/cids/custom/utils/WundaBlauServerResources.java
@@ -96,6 +96,7 @@ public enum WundaBlauServerResources {
     PRBR_CONF_PROPERTIES(new TextServerResource("/prbr/prbr_conf.properties")),
     BAUM_CONF_PROPERTIES(new TextServerResource("/baum/baum_conf.properties")),
     KLIMA_CONF_PROPERTIES(new TextServerResource("/klima/klima_conf.properties")),
+    KLIMAROUTE_CONF_PROPERTIES(new TextServerResource("/klimaroute/klimaroute_conf.properties")),
     NO2_CONF_PROPERTIES(new TextServerResource("/no2/no2_conf.properties")),
     STRADR_CONF_PROPERTIES(new TextServerResource("/stradr/stradr_conf.properties")),
 
