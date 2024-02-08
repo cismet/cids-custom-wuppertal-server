c82935bccb28728033197a45a42ad8277bd4f746
sandra-simmert <sandra.simmert@stadt.wuppertal.de>
2024-02-08 11:35:05 +0100

    ua Deletion Provider

---
+++ src/main/java/de/cismet/cids/custom/deletionprovider/UaLeistungenDeletionProvider.java
@@ -0,0 +1,97 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+package de.cismet.cids.custom.deletionprovider;
+
+import Sirius.server.localserver.object.AbstractCustomDeletionProvider;
+import Sirius.server.localserver.object.CustomDeletionProvider;
+import Sirius.server.localserver.object.DeletionProviderClientException;
+import Sirius.server.middleware.types.MetaObject;
+import Sirius.server.newuser.User;
+
+import org.apache.log4j.Logger;
+
+import org.openide.util.lookup.ServiceProvider;
+
+import java.rmi.RemoteException;
+
+import java.util.ArrayList;
+
+import de.cismet.cids.dynamics.CidsBean;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   sandra
+ * @version  $Revision$, $Date$
+ */
+@ServiceProvider(service = CustomDeletionProvider.class)
+public class UaLeistungenDeletionProvider extends AbstractCustomDeletionProvider {
+
+    //~ Static fields/initializers ---------------------------------------------
+
+    private static final Logger LOG = Logger.getLogger(UaLeistungenDeletionProvider.class);
+    private static final String TABLE_NAME = "ua_leistungen";
+    private static final String FIELD__ID = "id";
+    private static final String FIELD__FK = "fk_leistungen";
+    private static final String TABLE_NAME_SEARCH = "ua_einsatz_leistungen";
+    private static final String CAUSE =
+        "Diese Leistung kann nicht gelöscht werden, da diese bei mindestens einem Einsatz verwendet wird.";
+  
+
+    //~ Instance fields --------------------------------------------------------
+
+    private String deleteText = "Diese Leistung kann nicht gelöscht werden, da diese verwendet wird.";
+
+    //~ Methods ----------------------------------------------------------------
+
+    @Override
+    public String getTableName() {
+        return TABLE_NAME;
+    }
+
+    @Override
+    public boolean isMatching(final User user, final MetaObject metaObject) {
+        if (!super.isMatching(user, metaObject)) {
+            return false;
+        }
+
+        final CidsBean leistBean = metaObject.getBean();
+        final Integer leist_id = leistBean.getPrimaryKeyValue();
+
+        final String queryLeistIn = String.format(
+                "SELECT * FROM %s WHERE %s = %d;",
+                TABLE_NAME_SEARCH,
+                FIELD__FK,
+                leist_id);
+        
+        try {
+            final ArrayList<ArrayList> leistArray = getMetaService().performCustomSearch(
+                    queryLeistIn,
+                    getConnectionContext());
+            if (!leistArray.isEmpty()) {
+                deleteText = CAUSE;
+                return true;
+            }
+            
+        } catch (RemoteException ex) {
+            LOG.error("Cannot delete Leistung object", ex);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean customDeleteMetaObject(final User user, final MetaObject metaObject) throws Exception {
+        // darf nicht geloescht werden
+        throw new DeletionProviderClientException(deleteText);
+    }
+
+    @Override
+    public String getDomain() {
+        return "WUNDA_BLAU";
+    }
+}
