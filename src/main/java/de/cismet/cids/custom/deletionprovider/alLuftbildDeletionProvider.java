f23ca82f707be8dc7cca5364dee2bfb3d17fcfb3
sandra-simmert <sandra.simmert@stadt.wuppertal.de>
2024-02-20 09:12:22 +0100

    alLuftbildDeletionProvider

---
+++ src/main/java/de/cismet/cids/custom/deletionprovider/alLuftbildDeletionProvider.java
@@ -0,0 +1,61 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+package de.cismet.cids.custom.deletionprovider;
+
+import Sirius.server.localserver.object.AbstractCustomDeletionProvider;
+import Sirius.server.localserver.object.CustomDeletionProvider;
+import Sirius.server.localserver.object.DeletionProviderClientException;
+import Sirius.server.middleware.types.MetaObject;
+import Sirius.server.newuser.User;
+
+import org.apache.log4j.Logger;
+
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   sandra
+ * @version  $Revision$, $Date$
+ */
+@ServiceProvider(service = CustomDeletionProvider.class)
+public class alLuftbildDeletionProvider extends AbstractCustomDeletionProvider {
+
+    //~ Static fields/initializers ---------------------------------------------
+
+    private static final Logger LOG = Logger.getLogger(alLuftbildDeletionProvider.class);
+    public static final String TABLE_NAME = "al_luftbild";
+    private static final String DELETE_TEXT =
+        "Die Luftbilder können nicht gelöscht werden.";
+
+    //~ Methods ----------------------------------------------------------------
+
+    @Override
+    public String getTableName() {
+        return TABLE_NAME;
+    }
+
+    @Override
+    public boolean isMatching(final User user, final MetaObject metaObject) {
+        if (!super.isMatching(user, metaObject)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean customDeleteMetaObject(final User user, final MetaObject metaObject) throws Exception {
+        // darf nicht geloescht werden
+        throw new DeletionProviderClientException(DELETE_TEXT);
+    }
+
+    @Override
+    public String getDomain() {
+        return "WUNDA_BLAU";
+    }
+}
